aae589c7c9db48e25fae37a190bd0c74e54e4516
kenshi-miyabe <research@kenshi.miyabe.name>
2024-11-21 23:49:16 +0900

    update_answers.pyを書く

--- update_answers.py
+++ update_answers.py
@@ -6,10 +6,13 @@ grade_file = "./correct_answer/grade.csv"
 df = pd.read_csv(grade_file)
 
 # Q1からQ10の文字列を"-"で結合して新しいコラムに格納
-df['結合文字列'] = df[['Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Q7', 'Q8', 'Q9', 'Q10']].astype(str).agg('-'.join, axis=1)
+non_empty_rows = (df['結合文字列'].notna())
+
+df.loc[non_empty_rows,'結合文字列'] = df.loc[non_empty_rows,['Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Q7', 'Q8', 'Q9', 'Q10']].astype(int).astype(str).agg('-'.join, axis=1)
 
 # 結果を確認（必要に応じて）
+print(df['結合文字列'])
 print(df.head())
 
 # 必要なら新しいCSVに保存
-df.to_csv(grade_file, index=False)
+df.to_csv(grade_file, index=False, encoding='utf-8-sig')
