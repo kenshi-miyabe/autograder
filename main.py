2a329d81a27aa33a63f24cf1c480eb9fab1f68d7
Kenshi Miyabe <research@kenshi.miyabe.name>
2024-12-18 18:07:25 +0900

    72Bの利用，プロンプトの更新

--- main.py
+++ main.py
@@ -21,8 +21,10 @@ for file_name in sorted(os.listdir(dir_students)):
 # 画像からテキストを抽出
 problem_length = 50
 # モデル名、プロンプトを設定
-model_path_list = ["mlx-community/Qwen2-VL-7B-Instruct-4bit",
-                "mlx-community/pixtral-12b-4bit"]
+model_path_list = ["mlx-community/Qwen2-VL-72B-Instruct-4bit",
+                "mlx-community/pixtral-12b-8bit"]
+#                "mlx-community/Llama-3.2-11B-Vision-Instruct-8bit"]
+#                "mlx-community/Qwen2-VL-7B-Instruct-8bit"]
 model_name_list = ["Qwen2",
                 "Pixtral"]
 #mlx-community/Qwen2-VL-7B-Instruct-4bit
@@ -35,16 +37,14 @@ model_name_list = ["Qwen2",
 
 prompt_list = []
 prompt = """
-The answers to problems (1) through (50) are written as single-digit numbers.
-Provide only the answers in the format '(problem-number) digit'. Example:  
-===
-Answers:
+The answers to questions (1) through (50) must be written as single-digit numbers, exactly as shown in the image.
+Each answer must strictly adhere to the following format and be written on a separate line:
+(Question number) Answer's digit
+Example output (for illustration only):
 (1) 0
 (2) 0
 (3) 0
-...
-(50) 0
-===
+Ensure that the question number is enclosed in parentheses.
 """
 prompt_list.append(prompt)
 
@@ -65,19 +65,19 @@ for file_name in sorted(os.listdir(dir_students)):
 
 #テキストファイルからデータフレームの作成
 columns = ["学生番号"] + [f"Q{i:02}" for i in range(1, problem_length+1)]
-df1 = txt_to_df.construct_df(dir_students, "_page1-Qwen2.txt", columns, problem_length)
-df2 = txt_to_df.construct_df(dir_students, "_page1-Pixtral.txt", columns, problem_length)
-df_student = df1
+df0 = txt_to_df.construct_df(dir_students, "_page1-" + model_name_list[0] + ".txt", columns, problem_length)
+df1 = txt_to_df.construct_df(dir_students, "_page1-" + model_name_list[1] + ".txt", columns, problem_length)
+df_student = df0
 print(df_student.head())
 
 # 全ての列について値の違いを検出
-differences = (df1 != df2)
+differences = (df0 != df1)
 
 # 相違のある行と列を抽出
 if differences.any().any():
     diff_indices = differences[differences].stack().index.tolist()
     for row, col in diff_indices:
-        log = f"Diff at ID '{df1.at[row,"学生番号"]}', column '{col}': df1={df1.at[row, col]}, df2={df2.at[row, col]}"
+        log = f"Diff at ID '{df1.at[row,"学生番号"]}', column '{col}': df0={df0.at[row, col]}, df1={df1.at[row, col]}"
         print(log)
         mylib.log_error(log, file_name="./correct_answer/diff_log.txt")
 else:
@@ -86,6 +86,7 @@ else:
 # Excelファイルからデータフレームの作成
 report_excel = "./correct_answer/report_summary.xlsx"
 df_report = mylib.read_excel(report_excel, sheet_name=0, header=0, dtype=str)
+print(df_report.head())
 
 # マージ
 merged_df = pd.merge(df_report, df_student, on="学生番号", how="left")
