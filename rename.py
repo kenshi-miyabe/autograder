1211e7b374ca16a5ddd80fc643b710781c10fef6
Kenshi Miyabe <research@kenshi.miyabe.name>
2024-12-19 09:37:11 +0900

    rename追加

---
+++ rename.py
@@ -0,0 +1,19 @@
+import os
+
+target_substr = "_page1-Qwen2-72B"
+dir = './student_answers'
+
+for filename in os.listdir(dir):
+    filepath = os.path.join(dir, filename)
+    if os.path.isfile(filepath):
+        if target_substr in filename:
+            new_name = filename.replace(target_substr, "")
+            new_path = os.path.join(dir, new_name)
+            # リネーム
+            os.rename(filepath, new_path)
+            print(f"Renamed: {filename} -> {new_name}")
+        else:
+            # ファイル削除
+            os.remove(filepath)
+            print(f"Removed: {filename}")
+
